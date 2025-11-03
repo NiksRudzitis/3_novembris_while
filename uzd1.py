4109848c38bebe3593ec4a5a3fc6b99731ca262b
NiksRudzitis <niksrudz@madona.edu.lv>
2025-11-03 11:53:29 +0200

    local

--- uzd1.py
+++ uzd1.py
@@ -1,7 +1,7 @@
+# Summe ievaditos skaitlus 0 norada, ka darbs jabeidz
 summa = 0
 x = int(input("Ievadi skaitli (0 lai beigtu): "))
 while x != 0:
     summa += x
     x = int(input("Ievadi skaitli (0 lai beigtu): "))
 print("Summa:", summa)
-``
